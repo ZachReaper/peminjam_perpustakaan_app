c25a38e26797a4a18135cf63da57d1286c8406e9
rayhanwkwk <rayhanristyawan@gmail.com>
2024-01-18 13:12:24 +0700

    login page commit

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,7 +1,18 @@
+import 'package:dio/dio.dart' as dio;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_app/app/data/constant/endpoint.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/api_provider.dart';
 
 class RegisterController extends GetxController {
   //TODO: Implement RegisterController
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController telpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   final count = 0.obs;
   @override
@@ -20,4 +31,38 @@ class RegisterController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  final loadingBook = false.obs;
+  addRegister() async {
+    loadingBook(true);
+    try{
+      FocusScope.of(Get.context!).unfocus();
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.register,
+            data:
+            {
+              "nama": namaController.text.toString(),
+              "username": usernameController.text.toString(),
+              "telp": int.parse(telpController.text.toString()),
+              "alamat": alamatController.text.toString(),
+              "password": passwordController.text.toString(),
+            }
+        );
+        if (response.statusCode == 201) {
+          Get.back();
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loadingBook(false);
+    } on dio.DioException catch (e) {
+      loadingBook(false);
+      Get.snackbar("Sorry", e.message ?? "", backgroundColor: Colors.red);
+    } catch (e) {
+      loadingBook(false);
+      Get.snackbar("Error", e.toString(), backgroundColor: Colors.red);
+      throw Exception('Error: $e');
+    }
+  }
 }
--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -13,12 +13,67 @@ class RegisterView extends GetView<RegisterController> {
         title: const Text('RegisterView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'RegisterView is working',
-          style: TextStyle(fontSize: 20),
+      body: Form(
+        key: controller.formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller.namaController,
+              decoration: InputDecoration(hintText: "Masukkan Nama"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Nama tidak boleh kosong";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controller.usernameController,
+              decoration: InputDecoration(hintText: "Masukkan Username"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Username tidak boleh kosong";
+                }
+                return null;
+              },
+            ),TextFormField(
+              controller: controller.telpController,
+              decoration: InputDecoration(hintText: "Masukkan No telp"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "No telp tidak boleh kosong";
+                }
+                return null;
+              },
+            ),TextFormField(
+              controller: controller.alamatController,
+              decoration: InputDecoration(hintText: "Masukkan Alamat"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Alamat tidak boleh kosong";
+                }
+                return null;
+              },
+            ),TextFormField(
+              controller: controller.passwordController,
+              decoration: InputDecoration(hintText: "Masukkan Password"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Password tidak boleh kosong";
+                }
+                return null;
+              },
+            ),
+            Obx(() =>
+            controller.loadingBook.value?
+            CircularProgressIndicator():
+            ElevatedButton(onPressed: () {
+              controller.addRegister();
+            },
+                child: Text("Register")))
+          ],
         ),
-      ),
+      )
     );
   }
 }
